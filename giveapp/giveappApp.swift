fdaed1fdf1d5a2398b98e8663253f66aed4f3981
leonitejupi23 <leonitejupi23gmail.com>
2023-03-08 12:34:34 +0100

    Initial Commit

---
+++ giveapp/giveappApp.swift
@@ -0,0 +1,17 @@
+//
+//  giveappApp.swift
+//  giveapp
+//
+//  Created by Leonit Ejupi on 08.03.23.
+//
+
+import SwiftUI
+
+@main
+struct giveappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
