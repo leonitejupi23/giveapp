5c6fad6e18d1566da43a99a18f37c2200e0f448f
leonitejupi23 <leonitejupi23gmail.com>
2023-03-10 09:48:46 +0100

    give app using animations

---
+++ giveapp/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  giveapp
+//
+//  Created by Leonit Ejupi on 08.03.23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
